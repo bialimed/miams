1baf78c4ddca364aa215d2b0017d3edd5c03b278
fescudie <escudie.frederic@wanadoo.fr>
2018-03-12 15:15:39 +0100

    Add bin/msiMergeReports.py.

--- jflow/workflows/MSI/lib/msings.py
+++ jflow/workflows/MSI/lib/msings.py
@@ -42,15 +42,25 @@ def isGzip(path):
     return is_gzip
 
 
-class MsingsRecord:
-    def __init__(self, sample, score, status, loci=None):
-        self.sample = sample
+class MsingsSample:
+    def __init__(self, name, score, is_stable, loci=None):
+        """
+        @param name: [str] Name of the sample.
+        @param score: [float] Score of the MSI.
+        @param is_stable: [bool] True if the sample is stable.
+        @param loci: [dict] The stability status by locus (with True the locus is stable).
+        """
+        self.name = name
         self.score = score
-        self.status = status
+        self.is_stable = is_stable
         self.loci = dict() if loci is None else loci
 
 
     def getNbUnstable(self):
+        """
+        @summary: Returns the number of instable loci for the sample.
+        @return: [int] The number of instable loci.
+        """
         nb_unstable = 0
         for locus, is_stable in self.loci.items():
             if is_stable is not None and not is_stable:
@@ -59,6 +69,10 @@ class MsingsRecord:
 
 
     def getNbStable(self):
+        """
+        @summary: Returns the number of stable loci for the sample.
+        @return: [int] The number of stable loci.
+        """
         nb_stable = 0
         for locus, is_stable in self.loci.items():
             if is_stable is not None and is_stable:
@@ -66,49 +80,71 @@ class MsingsRecord:
         return nb_stable
 
 
-    def getNbEvaluated(self):
-        nb_evaluated = 0
+    def getNbUsable(self):
+        """
+        @summary: Returns the number of loci usable in MSI evaluation for the sample.
+        @return: [int] The number of instable loci.
+        """
+        nb_usable = 0
         for locus, is_stable in self.loci.items():
             if is_stable is not None:
-                nb_evaluated += 1
-        return nb_evaluated
+                nb_usable += 1
+        return nb_usable
 
 
     def getNbLoci(self):
+        """
+        @summary: Returns the number of loci in the sample.
+        @return: [int] The number of loci.
+        """
         return len(self.loci)
 
 
-class Report(object):
+class CountMSI(object):
     def __init__(self, filepath):
         """
         @param filepath: [str] The filepath.
         """
         self.filepath = filepath
-        self.records = dict()
+        self.samples = dict()
         self.loci = list()
         self.parse()
 
 
     def _parseFileHandle(self, FH):
-        samples = [elt.strip() for elt in FH.readline.split("\t")][1:]
-        nb_unstable = [elt.strip() for elt in FH.readline.split("\t")][1:]
-        nb_evaluated = [elt.strip() for elt in FH.readline.split("\t")][1:]
-        scores = [elt.strip() for elt in FH.readline.split("\t")][1:]
-        status = [elt.strip() for elt in FH.readline.split("\t")][1:]
+        # Parse general information
+        samples = [elt.strip() for elt in FH.readline().split("\t")][1:]
+        nb_unstable = [elt.strip() for elt in FH.readline().split("\t")][1:]
+        nb_evaluated = [elt.strip() for elt in FH.readline().split("\t")][1:]
+        scores = [elt.strip() for elt in FH.readline().split("\t")][1:]
+        for idx, elt in enumerate(scores):
+            curr_score = None
+            if elt != "":
+                curr_score = float(elt)
+            scores[idx] = curr_score
+        status = [elt.strip() for elt in FH.readline().split("\t")][1:]
+        for idx, elt in enumerate(status):
+            is_stable = None
+            if elt == "NEG":
+                is_stable = True
+            elif elt == "POS":
+                is_stable = False
+            status[idx] = is_stable
         for spl_idx, curr_spl in enumerate(samples):
-            self.records[curr_spl] = MsingsRecord(curr_spl, scores[spl_idx], status[spl_idx])
-        for curr_line in enumerate(FH):
+            self.samples[curr_spl] = MsingsSample(curr_spl, scores[spl_idx], status[spl_idx])
+        # Parse loci information
+        for curr_line in FH:
             fields = [elt.strip() for elt in curr_line.split("\t")]
             curr_locus = fields[0]
             self.loci.append(curr_locus)
-            for idx, curr_val in fields[1:]:
+            for idx, curr_val in enumerate(fields[1:]):
                 curr_spl = samples[idx]
                 if curr_val == "":
-                    self.records[curr_spl].loci[curr_locus] = None
+                    self.samples[curr_spl].loci[curr_locus] = None
                 elif curr_val == "1":
-                    self.records[curr_spl].loci[curr_locus] = True
+                    self.samples[curr_spl].loci[curr_locus] = False
                 else:
-                    self.records[curr_spl].loci[curr_locus] = False
+                    self.samples[curr_spl].loci[curr_locus] = True
 
 
     def parse(self):
