48c65b6991a4e6bb005c439b1bcc79b91fe047a6
fescudie <escudie.frederic@wanadoo.fr>
2018-07-24 09:46:51 +0200

    Add script for gather loci metrics and create MSISample.

---
+++ jflow/workflows/MIAmS_learn/bin/gatherLocusRes.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2018 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import json
+import argparse
+from anacore.msi import LocusRes, LocusResDistrib, LocusResPairsCombi, MSILocus, MSISample, MSIReport, Status
+from anacore.sv import HashedSVIO
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+def addLociDataFromFiles(msi_spl, in_loci_list, method_name, keys, res_cls):
+    """
+    Get selected data for loci of a sample and add them as data in LocusRes.
+
+    :param msi_spl: The sample where the results are added.
+    :type msi_spl: MSISample
+    :param in_loci_list: The path to the file containing the list of metrics files by locus (format: TSV). The header must be: #Locus_position<tab>Locus_name<tab>Filepath. Each file referenced in "Filepath" must be in JSON format and must contain a dictionary of metrics for one locus of the sample.
+    :type in_loci_list: str
+    :param method_name: The name of the method storing locus results in LocusRes.
+    :type method_name: str
+    :param keys: The keys extracted from in_locus_data and stored in LocusRes.
+    :type keys: dict (keys are name in in_locus_data and values are names in LocusRes.data)
+    :param res_cls: The class used to store LocusRes in msi_locus.
+    :type res_cls: LocusRes or one of its subclasses
+    """
+    with HashedSVIO(in_loci_list) as FH_loci_list:
+        for record in FH_loci_list:  # One file by locus
+            # Add locus
+            if record["Locus_position"] not in msi_spl.loci:
+                msi_spl.addLocus(
+                    MSILocus(record["Locus_position"], record["Locus_name"])
+                )
+            msi_locus = msi_spl.loci[record["Locus_position"]]
+            # Add result and data
+            addLociResult(msi_locus, record["Filepath"], method_name, keys, res_cls)
+
+
+def addLociResult(msi_locus, in_locus_data, method_name, keys, res_cls):
+    """
+    Get selected data from a JSON file and add them as data in a LocusRes.
+
+    :param msi_locus: The locus where the results are added.
+    :type msi_locus: MSILocus
+    :param in_locus_data: The path to the file containing the results for the locus (format: JSON).
+    :type in_locus_data: str
+    :param method_name: The name of the method storing locus results in LocusRes.
+    :type method_name: str
+    :param keys: The keys extracted from in_locus_data and stored in LocusRes.
+    :type keys: dict (keys are name in in_locus_data and values are names in LocusRes.data)
+    :param res_cls: The class used to store LocusRes in msi_locus.
+    :type res_cls: LocusRes or one of its subclasses
+    """
+    # Get data
+    with open(in_locus_data) as FH_locus:
+        locus_metrics = json.load(FH_locus)
+    # Add result
+    if method_name not in msi_locus.results:
+        msi_locus.results[method_name] = res_cls(Status.none)
+    locus_res = msi_locus.results[method_name]
+    # Add data
+    if keys is not None:  # Add selected keys
+        for metrics_key, result_key in keys.items():
+            locus_res.data[result_key] = locus_metrics[metrics_key]
+    else:  # Add all keys
+        for metrics_key, metrics_val in locus_metrics.items():
+            locus_res.data[metrics_key] = metrics_val
+
+
+def process(args):
+    """
+    Create MSISample from loci metrics.
+
+    :param args: The namespace extracted from the script arguments.
+    :type args: Namespace
+    """
+    spl_name = args.sample_name
+    if args.sample_name is None:
+        spl_name = os.path.basename(args.output_report).split(".")[0]
+        if spl_name.endswith("_report"):
+            spl_name = spl_name[:-7]
+    msi_spl = MSISample(spl_name)
+    # Add result data by loci
+    addLociDataFromFiles(msi_spl, args.input_loci_metrics_list, args.method_name, args.result_keys, args.method_class_name)
+    # Write report
+    MSIReport.write([msi_spl], args.output_report)
+
+
+class ResultKeysAction(argparse.Action):
+    """Manages results keys parameters."""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        res_by_metrics = dict()
+        print(values)
+        for curr in values:
+            metrics_key, result_key = curr.rsplit("=", 1)
+            res_by_metrics[metrics_key] = result_key
+        setattr(namespace, self.dest, res_by_metrics)
+
+
+class ResultClassName(argparse.Action):
+    """Manages result class name parameters."""
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        new_value = eval(value)
+        setattr(namespace, self.dest, new_value)
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Gather results about several locus in a MSISample object and write them in MSIReport.')
+    parser.add_argument('-s', '--sample-name', type=int, help='The name of the sample. [Default: output-report basename without extension]')
+    parser.add_argument('-n', '--method-name', default="model", help='The name of the method storing locus metrics in LocusRes. [Default: %(default)s]')
+    parser.add_argument('-c', '--method-class-name', default="LocusResDistrib", choices=["LocusRes", "LocusResDistrib", "LocusResPairsCombi"], action=ResultClassName, help='The class used to store LocusRes. [Default: %(default)s]')
+    parser.add_argument('-r', '--result-keys', nargs='+', action=ResultKeysAction, help='The keys retrieved from loci metrics and stored in LocusRes. Each key is couple with format "name_in_metrics=name_in_LocusRes_data". [Default: all tags in loci metrics]')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')  # Inputs
+    group_input.add_argument('-i', '--input-loci-metrics-list', required=True, help='The path to the file containing the list of metrics files by locus (format: TSV). The header must be: #Locus_position<tab>Locus_name<tab>Filepath. Each file referenced in "Filepath" must be in JSON format and must contain a dictionary of metrics for one locus of the sample.')
+    group_output = parser.add_argument_group('Outputs')  # Outputs
+    group_output.add_argument('-o', '--output-report', required=True, help='The path to the output file (format: JSON).')
+    args = parser.parse_args()
+
+    # Process
+    process(args)
---
+++ jflow/workflows/MIAmS_learn/components/gatherLocusRes.py
@@ -0,0 +1,94 @@
+#
+# Copyright (C) 2018 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2018 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+from jflow.component import Component
+from weaver.function import ShellFunction
+from anacore.bed import getAreas
+
+
+def two_dim_stack(elts, nb_col):
+    """
+    @summary: Returns 2D array from a 1D array. Each row contains nb_col columns.
+    @param elts: [list] The 1D array to process.
+    @param nb_col: [int] Number of columns in each rows of the 2D array.
+    @return: [list] The 2D array.
+    """
+    two_dim = list()
+    for elt_idx, curr_elt in enumerate(elts):
+        col_idx = elt_idx % nb_col
+        if col_idx == 0:
+            two_dim.append([curr_elt])
+        else:
+            two_dim[-1].append(curr_elt)
+    return two_dim
+
+
+class GatherLocusRes (Component):
+
+    def define_parameters(self, loci_reports, targets_design, samples_names, result_method, result_class_name="LocusResDistrib"):
+        # Parameters
+        self.add_parameter("result_method", "The name of the method storing locus metrics in LocusRes.", default=result_method)
+        self.add_parameter("result_class_name", "The class used to store LocusRes.", choices=["LocusRes", "LocusResDistrib", "LocusResPairsCombi"], default=result_class_name)
+        self.add_parameter_list("samples_names", "The samples names.", default=samples_names)
+
+        # Inputs files
+        self.add_input_file_list("loci_reports", 'Pathes to the file containing locus results (format: JSON). Each file contains the results for one locus of one sample. The list is ordered firstly by sample secondly by locus (locus are ordered by targets_design order).', default=loci_reports, required=True)
+        self.add_input_file("targets_design", 'Path to the file containing targets positions and names (format: BED).', default=targets_design, required=True)
+
+        # Outputs files
+        self.add_output_file_list("out_report", 'Pathes to the output reports (format: JSON). One report by sample.', pattern='{basename}_report.json', items=self.samples_names)
+        self.add_output_file_list("stderr", 'Pathes to the stderr files (format: txt).', pattern='{basename}.stderr', items=self.samples_names)
+
+
+    def process(self):
+        # Create combined lists
+        targets = getAreas(self.targets_design)
+        loci_reports_list_in_spl = list()
+        reports_in_spl = two_dim_stack(self.loci_reports, len(targets))
+        for spl_idx, reports in enumerate(reports_in_spl):
+            list_filepath = os.path.join(self.output_directory, "spl_{}_comb_reports_list.tsv".format(spl_idx))
+            loci_reports_list_in_spl.append(list_filepath)
+            with open(list_filepath, "w") as FH_out:
+                FH_out.write("#Locus_position\tLocus_name\tFilepath\n")
+                for target_idx, curr_report in enumerate(reports):
+                    target_id = "{}:{}-{}".format(targets[target_idx].chrom, targets[target_idx].start - 1, targets[target_idx].end)
+                    FH_out.write(
+                        "{}\t{}\t{}\n".format(target_id, targets[target_idx].name, curr_report)
+                    )
+        # Set commands
+        cmd = self.get_exec_path("gatherLocusRes.py") + \
+            " --method-name '{}'".format(self.result_method) + \
+            " --method-class-name '{}'".format(self.result_class_name) + \
+            " --result-keys 'nb_by_length=nb_by_length'" + \
+            " --input-loci-metrics-list $1" + \
+            " --output-report $2 " + \
+            " 2> $3"
+        report_fct = ShellFunction(cmd, cmd_format='{EXE} {IN} {OUT}')
+        for spl_idx, spl_name in enumerate(self.samples_names):
+            report_fct(
+                inputs=[loci_reports_list_in_spl[spl_idx]],
+                outputs=[self.out_report[spl_idx], self.stderr[spl_idx]],
+                includes=[reports_in_spl[spl_idx]]
+            )
